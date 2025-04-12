9d21cc4faa090232e54e6f9561143b337da94e69
MartinVilladsen <martin.k.villadsen@gmail.com>
2025-04-12 23:50:43 +0200

    vercel

--- app/Http/Controllers/SallingGroupController.php
+++ app/Http/Controllers/SallingGroupController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Services\ProductSuggestionService;
 use App\Services\SallingGroupService;
 
 class SallingGroupController extends Controller
